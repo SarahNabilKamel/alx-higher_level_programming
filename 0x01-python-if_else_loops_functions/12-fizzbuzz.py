cb9bbcc3bb61c4d1f493a180bc38418667d19fa9
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-10-03 23:06:42 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(i), end="")
