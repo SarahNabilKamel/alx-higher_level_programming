d7978d1f1653af6b0742842b62f0e38329d650a5
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-10-03 23:00:46 +0300

    Update 11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        n = chr(i)
-    else:
-        n = chr(i-32)
-    print("{}".format(n), end="")
+def pow(a, b):
+    return a**b
