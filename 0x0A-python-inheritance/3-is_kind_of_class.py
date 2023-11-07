51b02870f1d3e99705bca10d53b87660edf58766
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2023-11-07 03:16:41 +0200

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,4 +5,4 @@ Contains the is_kind_of_class function
 
 def is_kind_of_class(obj, a_class):
     """True if obj is an instance or inherited from a_class, else False"""
-    return (isinstance(obj, a_class))
+    return isinstance(obj, a_class)
