4dd35e443443954951bd930121eeb326814af440
Sarah Kamel <106119803+SarahNabilKamel@users.noreply.github.com>
2024-03-12 02:03:02 +0200

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
-3
-rts.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
+#!/usr/bin/node
+exports.callMeMoby = function (x, theFunction) {
+  for (let a = 0; a < x; a++) {
     theFunction();
   }
 };
